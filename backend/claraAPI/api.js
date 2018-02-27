5fb1f7875d57af1fe25b5f5e73e26c94041f69c2
alysaWSchool <woodhoua@mail.uoguelph.ca>
2018-02-27 10:32:21 -0500

    live graph data and clara icon changes

--- backend/claraAPI/api.js
+++ backend/claraAPI/api.js
@@ -1,4 +1,5 @@
 var express    = require('express');
+var cors = require('cors');
 var app        = express();
 var con        = require('./connected');
 var bodyParser = require('body-parser');
@@ -6,11 +7,17 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
 
 app.get('/tableLookUp', function(req, res){
     var id= req.param('id');
     var sql;
+
     if (id){
        sql= 'SELECT * FROM tableLookUp WHERE tableId= '+id
     }else{
@@ -29,7 +36,7 @@ app.get('/tableLookUp', function(req, res){
 app.get('/trafficCollisions', function(req, res){
 
     var sql= 'SELECT * FROM `Traffic Collisions` '
-     
+
     con.query(sql, function(err, rows){
        if(err){
            res.json({"Error": true, "Message":"Error Execute Sql"});
