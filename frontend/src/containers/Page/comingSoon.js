56de16fd5770b7dbb3fd4e414fabb0f7a7d02c3b
alysaWSchool <woodhoua@mail.uoguelph.ca>
2018-03-19 13:59:43 -0400

    fixed some routing issues

--- frontend/src/containers/Page/comingSoon.js
+++ frontend/src/containers/Page/comingSoon.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import Countdown from 'react-count-down';
-import Input from '../../components/uielements/input';
-import Button from '../../components/uielements/button';
+
+
 import Image from '../../image/claraCats04.svg';
-import IntlMessages from '../../components/utility/intlMessages';
+
 import FourZeroFourStyleWrapper from './404.style';
 
-const validateEmail = email => {
-  const regax = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regax.test(email);
-};
 
 class FourZeroFour extends React.Component {
-  state = {
-    email: '',
-    disabled: true
-  };
   render() {
     const options = {
       endDate: '03/01/2019 10:55 AM',
       prefix: 'until my birthday!'
     };
-    const { email, disabled } = this.state;
-    const onChange = event => {
-      const value = event.target.value;
-      this.setState({ disabled: !validateEmail(value), email: value });
-    };
+  
     return (
       <FourZeroFourStyleWrapper className="iso404Page">
         <div >
